3eb3caf77479f5677418ff5e0294f78b6f816c68
mferndel <mferndel@SVQ-5TJVS22.usersad.everis.int>
2021-05-20 01:39:08 +0200

    fixed mixed javadocs

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fpdualeveris"},{"l":"variables"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fpdualeveris"},{"l":"operators"}];updateSearchResults();
